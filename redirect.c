ef1e0c1212287a61cd1461be5044e0c18d9e5892
Renzo-Salosagcol <renzosalosagcol@outlook.com>
2024-12-16 13:21:23 -0800

    Added argc checking

--- redirect.c
+++ redirect.c
@@ -7,22 +7,27 @@
 #include <sys/wait.h>
 
 int main (int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <inp> <cmd> <out>\n", argv[0]);
+        return 1;
+    }
+
     int file_descriptors[2];
     pipe(file_descriptors);
     pid_t pid = fork();
 
     if (pid == 0) {
-      close(file_descriptors[1]);
+        close(file_descriptors[1]);
 
-      int output_file = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      char buffer[100];
-      ssize_t bytesRead;
-      while ((bytesRead = read(file_descriptors[0], buffer, sizeof(buffer))) > 0) {
-          write(output_file, buffer, bytesRead);
-      }
+        int output_file = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        char buffer[100];
+        ssize_t bytesRead;
+        while ((bytesRead = read(file_descriptors[0], buffer, sizeof(buffer))) > 0) {
+            write(output_file, buffer, bytesRead);
+        }
 
-      close(file_descriptors[0]);
-      close(output_file);
+        close(file_descriptors[0]);
+        close(output_file);
     } else {
         close(file_descriptors[0]);
 
